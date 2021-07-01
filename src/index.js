cb1b46accdcdee92246ead18a1e398cdcffc60c6
Anja Laube <mail@anjalaube.com>
2021-07-01 16:02:59 +0200

    last commit, time is up

--- src/index.js
+++ src/index.js
@@ -37,26 +37,26 @@ const pond1 = FilePond.create({
 })
 
 const pond2 = FilePond.create({
-   name: 'filter2',
-   allowImageFilter: true,
-   imageFilterColorMatrix: [
-     1.000,  0.000,  0.000,  0.000,  0.800,
-     0.200,  0.200,  0.300,  0.000,  0.000,
-     0.100,  0.000,  0.000,  0.000,  0.200,
-     0.000,  0.000,  0.000,  1.000,  0.000
-   ]
+  name: 'filter2',
+  allowImageFilter: true,
+  imageFilterColorMatrix: [
+    1.000, 0.000, 0.000, 0.000, 0.800,
+    0.200, 0.200, 0.300, 0.000, 0.000,
+    0.100, 0.000, 0.000, 0.000, 0.200,
+    0.000, 0.000, 0.000, 1.000, 0.000
+  ]
 })
 
 const pond3 = FilePond.create({
-   name: 'filter3',
-   allowImageFilter: true,
-   imageFilterColorMatrix: [
-     0.600,  0.000,  0.300,  0.000,  0.000,
-     0.200,  0.000,  0.400,  0.000, 0.000,
-     0.100,  0.000,  0.700,  0.000,  0.000,
-     0.000,  0.000,  0.000,  1.000,  0.000
-   ]
- })
+  name: 'filter3',
+  allowImageFilter: true,
+  imageFilterColorMatrix: [
+    0.600, 0.000, 0.300, 0.000, 0.000,
+    0.200, 0.000, 0.400, 0.000, 0.000,
+    0.100, 0.000, 0.700, 0.000, 0.000,
+    0.000, 0.000, 0.000, 1.000, 0.000
+  ]
+})
 
 // all instances process same file
 pond.on('addfile', () => {
@@ -73,7 +73,6 @@ pond.on('addfile', () => {
 const carousel = document.getElementById('carousel')
 const uploadArea = document.getElementById('uploadArea')
 const downloadArea = document.getElementById('downloadArea')
-const downloadButton = document.getElementById('download')
 const restartButton = document.getElementById('restart')
 let carouselSplide
 
@@ -84,7 +83,7 @@ uploadArea.appendChild(pond.element)
 
 // Add instances to hidden div to the DOM
 const hiddenDiv = document.getElementById('hidden-div')
-hiddenDiv.classList.add('hidden')
+// hiddenDiv.classList.add('hidden')
 hiddenDiv.appendChild(pond1.element)
 hiddenDiv.appendChild(pond2.element)
 hiddenDiv.appendChild(pond3.element)
@@ -92,13 +91,13 @@ hiddenDiv.appendChild(pond3.element)
 
 // method to add selected image to slider
 const addImage = (image) => {
-  if (!image) return
+  // if (!image) return
   const slide = document.createElement('div')
   carousel.classList.remove('hidden')
   slide.classList.add('splide__slide')
   slide.appendChild(image)
   carouselSplide.add(slide)
-  hiddenDiv.classList.add('hidden')
+  // hiddenDiv.classList.add('hidden')
 }
 
 
@@ -108,7 +107,7 @@ uploadButton.innerHTML = 'Upload'
 uploadButton.classList.add('custom')
 uploadArea.appendChild(uploadButton)
 
-uploadButton.addEventListener ('click', () => {
+uploadButton.addEventListener('click', () => {
   addImage(pond.element.querySelector('canvas'))
   addImage(pond1.element.querySelector('canvas'))
   addImage(pond2.element.querySelector('canvas'))
@@ -124,21 +123,8 @@ uploadButton.addEventListener ('click', () => {
 })
 
 
-// // download selected image
-// click event on splide_slide on Focus/in Center
-downloadButton.addEventListener(('click'), () => {
-  // const downloadArea = document.getElementById('downloadArea')
-  const link = document.createElement('a')
-  link.download = 'filename.png'
-  link.href = document.querySelector('canvas').toDataURL()
-  link.click()
-  // let chosenImage = document.getElementById('carousel-slide01').toDataURL()
-  // chosenImage.click()
-})
-
-
 //restart programm --- Doenst work (pond needs to be restarted(preview image removed))
-restartButton.addEventListener ('click', () => {
+restartButton.addEventListener('click', () => {
   pond.removeFiles()
 
   const carousel = document.getElementById('carousel')
